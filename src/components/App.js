8a63ba626fef918f13953a4d29e9644a5d091471
maccorzo <marko@hankkila.me>
2018-10-13 14:36:18 +0300

    Add state

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,19 @@ import '../static/css/style.css';
 import Board from './Board'
 
 class App extends Component {
+  state = {
+    activeNumber: 1042,
+    largestNumber: 1001,
+    queue: [],
+  }
+
   render() {
     return (
       <div className="App">
-        <header>link link link</header>
+        <header className="nav">link link link</header>
         <Switch>
           <Route exact path="/">
-            <Board />
+            <Board activeNumber={this.state.activeNumber} />
           </Route>
           <Route exact path="/about">
             <div>Route /about</div>
