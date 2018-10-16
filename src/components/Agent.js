3af195e8d4d6ea7a536209f19cccd8cbe48f1d8a
maccorzo <marko@hankkila.me>
2018-10-16 20:07:01 +0300

    Show real queue length

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 class Agent extends React.Component {
+
   render() {
     return (
       <form className="agent">
         <div>Next </div>
         <div className="agent__input" id="next">042</div>
-        <div>Queue</div><div>3</div>
+        <div>Queue</div><div>{Object.keys(this.props.queue).length}</div>
         <button className="agent__button" type="submit">Take the next customer</button>
       </form >
     )
